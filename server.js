a58eeb7cb326c497b6a8b573624a73c61044f5fa
Srikanth K Hari <srikanth@vidao.com>
2015-02-20 12:41:24 -0800

    Tests e2e and server

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ app.use('/api/users', require('./controller/api/users'));
 app.use('/api/posts', require('./controller/api/posts'));
 app.use( require('./controller/static'));
 
-var server = app.listen(8081, function () {
-	console.log('Server listening on', 8081)
-})
-require('./websockets').connect(server)
+var port = process.env.PORT || 8083;
+var server = app.listen(port, function () {
+	console.log('Server', process.pid, 'listening on', port);
+});
+require('./websockets').connect(server);
