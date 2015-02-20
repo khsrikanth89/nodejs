9c480ce5c2aabd52904951a99b7b20c763e6b024
khsrikanth89 <khsrikanth89@gmail.com>
2015-02-20 15:34:25 -0800

    Changes

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ app.use('/api/sessions', require('./controller/api/sessions'));
 app.use('/api/users', require('./controller/api/users'));
 app.use('/api/posts', require('./controller/api/posts'));
 app.use( require('./controller/static'));
-
 var port = process.env.PORT || 8083;
 var server = app.listen(port, function () {
 	console.log('Server', process.pid, 'listening on', port);
